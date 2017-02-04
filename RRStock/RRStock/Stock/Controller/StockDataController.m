ca8733794a9cedf2c240402efe51255b5f98752b
qingluanchou <zengwenliang@bogon>
2017-02-04 22:14:18 +0800

    1.ttt

--- RRStock/RRStock/Stock/Controller/StockDataController.m
+++ RRStock/RRStock/Stock/Controller/StockDataController.m
@@ -19,6 +19,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    
+    
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
     
